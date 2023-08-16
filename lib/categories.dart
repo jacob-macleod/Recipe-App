2a1f3ef91e2aa60d0ac4e898e5fad76e6665ab70
jacob-macleod <jacob.e.macleod@gmail.com>
2023-08-16 11:49:37 +0100

    Added grid view

--- lib/categories.dart
+++ lib/categories.dart
@@ -9,6 +9,21 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Pick your category"),
       ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text("Category 1", style: TextStyle(color: Colors.white)),
+          Text("Category 2", style: TextStyle(color: Colors.white)),
+          Text("Category 3", style: TextStyle(color: Colors.white)),
+          Text("Category 4", style: TextStyle(color: Colors.white)),
+          Text("Category 5", style: TextStyle(color: Colors.white)),
+        ],
+      ),
     );
   }
 }
