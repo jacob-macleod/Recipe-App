6f604136b7da02a8d0835fe710cd6a56b8076be9
jacob-macleod <jacob.e.macleod@gmail.com>
2023-09-13 20:04:09 +0100

    Added more items on meal item widgets

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -8,6 +8,16 @@ class MealItem extends StatelessWidget {
   const MealItem(this.meal, {super.key});
   final Meal meal;
 
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
+
   @override
   Widget build(context) {
     return Card(
@@ -51,10 +61,21 @@ class MealItem extends StatelessWidget {
                     ),
                     const SizedBox(height: 12),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         MealItemTrait(
                           icon: Icons.schedule,
                           label: '${meal.duration} min',
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
                         ),
                       ],
                     )
