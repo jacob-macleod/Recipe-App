5e3046a4f8db3acda5ef7c9bc44ae7544a4fe2b5
jacob-macleod <jacob.e.macleod@gmail.com>
2023-09-13 19:59:36 +0100

    Added time requirement to meals

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:transparent_image/transparent_image.dart';
 
+import 'meal_item_trait.dart';
+
 class MealItem extends StatelessWidget {
   const MealItem(this.meal, {super.key});
   final Meal meal;
@@ -49,7 +51,12 @@ class MealItem extends StatelessWidget {
                     ),
                     const SizedBox(height: 12),
                     Row(
-                      children: [],
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                      ],
                     )
                   ],
                 ),
